66a08265c503e4624fb8a1cccd0d220cfd4f6cfd
TDhananjaya <tharindudhananjaya1122@gmail.com>
2019-01-05 20:11:29 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose =require("mongoose");
 
 const adminRoutes = require("./routes/admin-routes");
 const userRoutes = require("./routes/user-routes");
 const clientRoutes =require("./routes/client-routes");
 const db = require("./lib/db");
 const app = express();
+
+// mongodb://<dbuser>:<dbpassword>@ds149404.mlab.com:49404/foodordering
+// mongoose.connect('mongodb://gimhana329:nov02329@ds245532.mlab.com:45532/travellia',
+
+// mongoose.connect('mongodb://tharindud:123456789qaz@ds149404.mlab.com:49404/foodordering', function(err){
+//     if(err) {
+//         console.log('Some problem with the connection ' +err);
+//     } else {
+//         console.log('The Mongoose connection is ready');
+//     }
+// })
 
 const config = require("./config.json")[app.get("env")];
 
